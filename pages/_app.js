2cd832ea45c6a00f1516f3ec39afb780656ff731
brokoyy <bz523533@gmail.com>
2025-08-29 23:42:10 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 // pages/_app.js
-import "../styles/globals.css";
+import '../styles/globals.css'
+import { NostrProvider } from '../lib/nostr'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NostrProvider>
+      <Component {...pageProps} />
+    </NostrProvider>
+  )
 }
