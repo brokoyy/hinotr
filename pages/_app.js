846ed02e7672dc91b36faf64a9d351bde6e05e14
brokoyy <bz523533@gmail.com>
2025-08-29 21:15:49 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,11 @@
-import '../styles/globals.css'
-import { useState } from 'react'
-import { NostrProvider } from '../lib/nostr'
+// pages/_app.js
+import { NostrProvider } from "../lib/nostr";
+import "../styles/globals.css";
 
-
-export default function App({ Component, pageProps }) {
-const [pubkey, setPubkey] = useState(null)
-const [relays, setRelays] = useState([
-'wss://relay.damus.io',
-'wss://nostr-pub.wellorder.net'
-])
-
-
-return (
-<NostrProvider value={{ pubkey, setPubkey, relays, setRelays }}>
-<Component {...pageProps} />
-</NostrProvider>
-)
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <NostrProvider>
+      <Component {...pageProps} />
+    </NostrProvider>
+  );
 }
